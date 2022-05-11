429815597e7aa338105e236d880e2221326beb61
suman-2902 <102145634+suman-2902@users.noreply.github.com>
2022-05-11 19:55:25 +0530

    Create variables.cpp

---
+++ variables/variables.cpp
@@ -0,0 +1,11 @@
+  #include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    double b;
+    char d;
+    string s;
+    bool h;
+    cin>>a>>b>>d>>s>>h;
+    cout<<a<<"\n"<<b<<"\n"<<d<<"\n"<<s<<"\n"<<h;
+}
